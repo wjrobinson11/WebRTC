b8ee63f6dc127808bfc571b810842739d8a97b5a
wesrobinson <wes@keystacksolutions.com>
2018-01-29 04:27:11 -0600

    Webrtc

---
+++ db/migrate/20180129094261_add_slug_to_talking_stick_rooms.talking_stick.rb
@@ -0,0 +1,12 @@
+# This migration comes from talking_stick (originally 20150722200822)
+class AddSlugToTalkingStickRooms < ActiveRecord::Migration[4.2]
+  def up
+    add_column :talking_stick_rooms, :slug, :string
+    # Add slugs to the existing rooms
+    TalkingStick::Room.all.map &:save
+  end
+
+  def down
+    remove_column :talking_stick_rooms, :slug
+  end
+end
